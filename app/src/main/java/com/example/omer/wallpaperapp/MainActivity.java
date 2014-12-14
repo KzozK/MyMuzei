b07942a9478af4d71eb37415880948c6c7f9d278
omer ozkul <omer.ozkul@epitech.eu>
2014-12-14 19:30:53 +0100

    first commit

---
+++ app/src/main/java/com/example/omer/wallpaperapp/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.omer.wallpaperapp;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.format.Time;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.WallpaperManager;
+import java.io.IOException;
+import android.widget.Toast;
+import android.graphics.drawable.Drawable;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.graphics.BitmapFactory;
+
+public class MainActivity extends Activity {
+
+    public void setWallPaper(View view) {
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
+        try {
+
+            Drawable currentWP = wallpaperManager.getDrawable();
+            int height = currentWP.getIntrinsicHeight();
+            int width = currentWP.getIntrinsicWidth();
+
+            Bitmap rome = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.rome);
+
+            Bitmap bitmap = Bitmap.createScaledBitmap(rome, width, height, true);
+            wallpaperManager.setBitmap(bitmap);
+
+            Toast toast = Toast.makeText(this, "Set wallpaper successfully!", Toast.LENGTH_LONG);
+            toast.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startService(View view)
+    {
+        Log.d("MES LOOOGGG", "start service");
+        Intent wpService = new Intent(this, WallpaperIntentService.class);
+        wpService.putExtra("url","www.toto.com");
+        startService(wpService);
+        Toast toast = Toast.makeText(this, "service started successfully!", Toast.LENGTH_LONG);
+        toast.show();
+        finish();
+    }
+/*
+    public void btnCancelSchedules(View v) {
+
+        Intent intent = new Intent(getApplicationContext(), AlaramReceiver.class);
+        intent.putExtra(AlaramReceiver.ACTION_ALARM, AlaramReceiver.ACTION_ALARM);
+
+        final PendingIntent pIntent = PendingIntent.getBroadcast(this, 1234567,
+                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        AlarmManager alarms = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        alarms.cancel(pIntent);
+    }*/
+
+    private void downloadImage()
+    {/*
+        String name = c.getString(str_url);
+        URL url_value = new URL(name);
+        ImageView profile = (ImageView)v.findViewById(R.id.vdo_icon);
+        if (profile != null) {
+            Bitmap mIcon1 =
+                    BitmapFactory.decodeStream(url_value.openConnection().getInputStream());
+            profile.setImageBitmap(mIcon1);
+        }*/
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // startService(new View(getApplicationContext()));
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/omer/wallpaperapp/WallpaperIntentService.java
@@ -0,0 +1,162 @@
+package com.example.omer.wallpaperapp;
+
+import android.app.AlarmManager;
+import android.app.IntentService;
+import android.app.WallpaperManager;
+import android.content.Intent;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.widget.Toast;
+import android.os.Handler;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.io.IOException;
+import android.text.format.Time;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class WallpaperIntentService extends IntentService {
+    // TODO: Rename actions, choose action names that describe tasks that this
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_FOO = "com.example.omer.wallpaperapp.action.FOO";
+    private static final String ACTION_BAZ = "com.example.omer.wallpaperapp.action.BAZ";
+
+    // TODO: Rename parameters
+    private static final String EXTRA_PARAM1 = "com.example.omer.wallpaperapp.extra.PARAM1";
+    private static final String EXTRA_PARAM2 = "com.example.omer.wallpaperapp.extra.PARAM2";
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionFoo(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, WallpaperIntentService.class);
+        intent.setAction(ACTION_FOO);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    /**
+     * Starts this service to perform action Baz with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionBaz(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, WallpaperIntentService.class);
+        intent.setAction(ACTION_BAZ);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    public WallpaperIntentService() {
+        super("WallpaperIntentService");
+    }
+
+    private static boolean isFirstTime = true;
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d("SERVICEEEEEE", "onHandleIntent()");
+
+        //startTimer();
+
+        //String dataString = workIntent.getDataString();
+
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_FOO.equals(action)) {
+                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
+                handleActionFoo(param1, param2);
+            } else if (ACTION_BAZ.equals(action)) {
+                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
+                handleActionBaz(param1, param2);
+            }
+        }
+        setWallPaper();
+        if (isFirstTime) {
+            nextWPChange(intent);
+            isFirstTime = false;
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionFoo(String param1, String param2) {
+        // TODO: Handle action Foo
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionBaz(String param1, String param2) {
+        // TODO: Handle action Baz
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    private void nextWPChange(Intent theIntent) {
+
+        try {
+            PendingIntent pendingIntent = PendingIntent.getService(this, 0, theIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            long currentTimeMillis = System.currentTimeMillis();
+            AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+//        alarmManager.set(AlarmManager.RTC, currentTimeMillis + (5 * 1000), pendingIntent);
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, 0, currentTimeMillis + (5 * 1000), pendingIntent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+        private static boolean state = true;
+
+    public void setWallPaper() {
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
+        try {
+
+            Drawable currentWP = wallpaperManager.getDrawable();
+            int height = currentWP.getIntrinsicHeight();
+            int width = currentWP.getIntrinsicWidth();
+
+            Bitmap rome;
+            if (state) {
+                rome = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.rome);
+                state = false;
+            }
+            else {
+                rome = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.ggstyle);
+                state = true;
+            }
+
+            Bitmap bitmap = Bitmap.createScaledBitmap(rome, width, height, true);
+            wallpaperManager.setBitmap(bitmap);
+
+            Toast toast = Toast.makeText(this, "Set wallpaper on BG successfully!", Toast.LENGTH_LONG);
+            toast.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
